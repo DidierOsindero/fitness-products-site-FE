c9b6d376b3eb43b3379da8ffc8a53c7e0c0e15a7
Didier Osindero <didierosindero@gmail.com>
2023-01-11 08:40:56 +0000

    Fetch data from backend and render sales products

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,44 @@
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { baseURL } from "../App";
+import { IFitnessProduct } from "../interfaces";
+import { Product } from "./Product";
+
 export const Home = (): JSX.Element => {
-  return (
-    <div className="ctn-home">
-      <div className="home-hero">
-        <h1 className="home-hero-txt">Smart Fitness</h1>
+  const [saleProductsArr, setSaleProductsArr] = useState<IFitnessProduct[]>([]);
+
+  const getSaleProducts = useCallback(async () => {
+    try {
+      const { data } = await axios.get(baseURL + "/products/sale");
+      setSaleProductsArr(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [setSaleProductsArr]);
+
+  useEffect(() => {
+    getSaleProducts();
+  }, [getSaleProducts]);
+
+  if (saleProductsArr.length < 1) {
+    return (
+      <div className="ctn-home">
+        <div className="home-hero">
+          <h1 className="home-hero-txt">Smart Fitness</h1>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="ctn-home">
+        <div className="home-hero">
+          <h1 className="home-hero-txt">Smart Fitness</h1>
+        </div>
+        <div className="sale-products-block">
+          <Product product={saleProductsArr[0]} />
+          <Product product={saleProductsArr[1]} />
+        </div>
+      </div>
+    );
+  }
 };
