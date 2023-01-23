b2f19baa5110bc990252be64c39727878392bcbc
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-01-23 10:31:43 +0000

    fetch brand names from backend

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -2,3 +2,11 @@ export interface ISearchBar {
   searchInput: string;
   handleSeachInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
+
+export interface IBrandNames {
+  brand_name: string;
+}
+
+export interface IBrandsView {
+  brandNames: IBrandNames[];
+}
