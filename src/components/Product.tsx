516ac19a90aac9ab9c62b3d7011657cd8fd01150
Didier Osindero <didierosindero@gmail.com>
2023-01-24 08:31:53 +0000

    Add img tag to product

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -6,9 +6,16 @@ interface IProductProps {
 export const Product = ({ product }: IProductProps): JSX.Element => {
   return (
     <div className="ctn-product">
-      <p className="product-model">{product.model_name}</p>
-      <p className="product-brand">{product.brand_name}</p>
-      <p className="product-selling-price">{product.selling_price}</p>
+      <img
+        className="product-img"
+        src="https://images.unsplash.com/photo-1579586337278-3befd40fd17a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c21hcnQlMjB3YXRjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+      />
+      <div className="ctn-product-description">
+        <p className="product-model">{product.model_name}</p>
+        <p className="product-brand">{product.brand_name}</p>
+        <p className="product-selling-price">{product.selling_price}</p>
+      </div>
+      <button>Buy</button>
     </div>
   );
 };
