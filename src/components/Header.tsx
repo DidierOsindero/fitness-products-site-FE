fb1a47f214f41e94b7cec2a0ee77604bcfd3b23a
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-02-03 13:37:12 +0000

    add active class names to react router nav links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export const Header = (): JSX.Element => {
   return (
     <div className="headerWrapper">
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="brandnames">Brands</NavLink>
+      <nav className="nav-bar">
+        <NavLink
+          className={({ isActive }) => (isActive ? "active" : "navLink")}
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active" : "navLink")}
+          to="brandnames"
+        >
+          Brands
+        </NavLink>
       </nav>
     </div>
   );
