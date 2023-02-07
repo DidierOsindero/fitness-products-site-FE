ced6dc6458820fcfd3b53d93238fc2ec8c8392c6
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-02-07 13:22:28 +0000

    attach correct image to each singlebrand

--- src/components/BrandsView.tsx
+++ src/components/BrandsView.tsx
@@ -4,8 +4,8 @@ import { SingleBrand } from "./SingleBrand";
 export function BrandsView({ brandNames }: IBrandsView): JSX.Element {
   return (
     <div className="BrandsViewContainer">
-      {brandNames.map((brand) => {
-        return <SingleBrand brand={brand} key={brand.brand_name} />;
+      {brandNames.map((brand, i) => {
+        return <SingleBrand brand={brand} key={brand.brand_name} index={i} />;
       })}
     </div>
   );
